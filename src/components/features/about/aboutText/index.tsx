9d160d570f3af329d82ffabbd7f3d1f87a63ec53
smmuja <smmuja27@gmail.com>
2024-06-25 14:18:46 +0700

    update: about content

--- src/components/features/about/aboutText/index.tsx
+++ src/components/features/about/aboutText/index.tsx
@@ -3,7 +3,7 @@ import { Emoji } from "components";
 
 import catImgUrl from "assets/img/catCode.png";
 
-export function AboutTry() {
+export function AboutText() {
   return (
     <>
       <SectionCard imgUrl={""} title={""}>
@@ -23,27 +23,23 @@ export function AboutTry() {
 
         <Emoji />
       </SectionCard>
-      <SectionCard imgUrl={""} title={""}>
-        <ul className="flex flex-col list-none gap-5">
-          <li>
-            🎓 Recently graduated from
-            <b> Revou Full Stack Software Engineering </b>
-            program
-          </li>
-          <li>
-            💻 Starting with <b>HTML, CSS, JavaScript</b> trio and diving deep
-            into Front-end framework such as <b>ReactJs</b> and <b>NextJs </b>
-          </li>
-          <li>
-            🏢 Having fundamental experience in <b>Python, Flask, MySQL </b> for
-            Backend, might extend to Backend development as seniority level up
-          </li>
-          <li>
-            🎯 I am particularly interested in the <b> Front-end</b> development
-            to bridge between business goals and customers' needs and am eager
-            to contribute to this field.
-          </li>
-        </ul>
+      <SectionCard title={""} imgUrl={""}>
+        🎓 Recently graduated from
+        <b> Revou Full Stack Software Engineering </b>
+        program
+      </SectionCard>
+      <SectionCard title={""} imgUrl={catImgUrl}>
+        💻 Starting with <b>HTML, CSS, JavaScript</b> trio and diving deep into
+        Front-end framework such as <b>ReactJs</b> and <b>NextJs </b>
+      </SectionCard>
+      <SectionCard title={""} imgUrl={""}>
+        🏢 Having fundamental experience in <b>Python, Flask, MySQL </b> for
+        Backend, might extend to Backend development as seniority level up
+      </SectionCard>
+      <SectionCard title={""} imgUrl={catImgUrl}>
+        🎯 I am particularly interested in the <b> Front-end</b> development
+        tobridge between business goals and customers' needs and am eager to
+        contribute to this field.
       </SectionCard>
     </>
   );
