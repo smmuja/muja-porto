dfdf0ee9f98e0e3fe939b1e20b5d6ae5311be827
smmuja <smmuja27@gmail.com>
2024-06-27 02:58:03 +0700

    fix sidebar on mobile

--- src/layouts/default-layouts/components/sidebar/components/sidebarWrapper/index.tsx
+++ src/layouts/default-layouts/components/sidebar/components/sidebarWrapper/index.tsx
@@ -12,7 +12,7 @@ import {
 
 export function SidebarWrapper() {
   return (
-    <nav className="flex flex-col p-3 gap-5 bg-fuchsia-950 text-gray-400 overflow-y-scroll sm:w-2/5 lg:w-1/5 h-screen max-h-screen ">
+    <nav className="flex flex-col p-3 gap-5 bg-fuchsia-950 text-gray-400 overflow-y-scroll w-20 sm:w-2/5 lg:w-1/5 h-screen max-h-screen ">
       {/* <Head /> */}
       <SearchBar />
       <Home />
