a23d87dfc7adb9ca111253d4bb116f2c866ecafd
smmuja <smmuja27@gmail.com>
2024-06-29 14:07:00 +0700

    minor fix overflow

--- src/layouts/default-layouts/components/sidebar/sidebarWrapper/index.tsx
+++ src/layouts/default-layouts/components/sidebar/sidebarWrapper/index.tsx
@@ -2,7 +2,7 @@ import { PortfolioTitle, SidebarNav } from "layouts/default-layouts";
 
 export function SidebarWrapper() {
   return (
-    <div className="hidden sm:flex  flex-col m-0 p-3 border-0 bg-fuchsia-950 text-gray-400   overflow-y-scroll w-20 sm:w-2/5 md:2/5 lg:w-1/5 h-screen max-h-screen">
+    <div className="hidden sm:flex  flex-col m-0 p-3 border-0 bg-fuchsia-950 text-gray-400  overflow-y-scroll w-20 sm:w-2/5 md:2/5 lg:w-1/5 ">
       <PortfolioTitle className="hidden sm:inline" />
       <SidebarNav />
     </div>
