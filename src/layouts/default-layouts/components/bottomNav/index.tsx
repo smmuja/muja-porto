8258c0336d722fd342da68bdc67c95a2c38bdbd8
smmuja <smmuja27@gmail.com>
2024-06-29 14:22:25 +0700

    minor fix bottom nav

--- src/layouts/default-layouts/components/bottomNav/index.tsx
+++ src/layouts/default-layouts/components/bottomNav/index.tsx
@@ -3,7 +3,7 @@ import { navPath } from "config/navPath";
 
 export function BottomNav() {
   return (
-    <nav className="sm:hidden flex sticky bottom-0 gap-0  border-t justify-center overflow-x-scroll bg-white">
+    <nav className="sm:hidden flex sticky bottom-0 gap-0 px-0 py-3 border-t text-center justify-center items-center  content-center overflow-x-scroll bg-white">
       {navPath.map((item) => {
         return (
           <>
