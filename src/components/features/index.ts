d0252551c58dfb7df853068b8ae92254a968baf1
smmuja <134966551+smmuja@users.noreply.github.com>
2024-07-08 15:40:28 +0700

    Revert "Add right side bar"
    This reverts commit 49fc3182cd092e08254a354dac2614ced4d81136.

--- src/components/features/index.ts
+++ src/components/features/index.ts
@@ -2,7 +2,6 @@ export * from "./home";
 export * from "./about";
 export * from "./education";
 export * from "./work";
-export * from "./project";
 export * from "./skill";
 export * from "./contact";
-export * from "./404";
+export * from "./project";
