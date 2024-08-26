732a0cb3f6eb779271ec9413b9bbe4b47fa0f97f
smmuja <smmuja27@gmail.com>
2024-08-26 18:25:21 +0700

    feat(RightSidebar): setup Right Sidebar hooks and context

--- src/layouts/default-layouts/components/rightSidebar/index.tsx
+++ src/layouts/default-layouts/components/rightSidebar/index.tsx
@@ -1,5 +1,6 @@
 import {
   AboutSnippet,
+  Button,
   ButtonViewAbout,
   Card,
   Emoji,
@@ -7,21 +8,36 @@ import {
 } from "components";
 
 import userImg from "assets/img/user.png";
+import { useOpenRightSidebarContext } from "providers/OpenRightSidebarContext";
 
 export function RightSidebar() {
-  return (
-    <Card className="border-0 hidden flex-col w-full overflow-hidden">
-      <div className="flex gap-3">
-        <img src={userImg} alt="" className="size-7 rounded-md" />
-        <h3 className="font-semibold m-0 text-lg">Muja Portfolio</h3>
-      </div>
+  const { handleCloseRightSidebar, isRightSidebarOpen } =
+    useOpenRightSidebarContext();
 
-      <Card className="m-5 p- border-0">
-        <ProfileSnippet />
-        <AboutSnippet />
-        <Emoji />
-        <ButtonViewAbout />
-      </Card>
-    </Card>
+  return (
+    <>
+      {isRightSidebarOpen && (
+        <Card
+          className={`border-0 hidden md:flex flex-col md:w-full lg:w-1/2 overflow-hidden`}
+        >
+          <div className="flex justify-between">
+            <div className="flex gap-3">
+              <img src={userImg} alt="" className="size-7 rounded-md" />
+              <h3 className="font-semibold m-0 text-lg">Muja Portfolio</h3>
+            </div>
+            <Button onClick={() => handleCloseRightSidebar()} className="m-0">
+              X
+            </Button>
+          </div>
+
+          <Card className="m-5 p- border-0">
+            <ProfileSnippet />
+            <AboutSnippet />
+            <Emoji />
+            <ButtonViewAbout />
+          </Card>
+        </Card>
+      )}
+    </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes.ts";
+import { OpenRightSidebarProvider } from "providers/OpenRightSidebarContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={routes} />
+    <OpenRightSidebarProvider>
+      <RouterProvider router={routes} />
+    </OpenRightSidebarProvider>
   </React.StrictMode>
 );
