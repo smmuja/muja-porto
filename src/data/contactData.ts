360091ff1a286ae70b6720347367ba8f549854ff
smmuja <smmuja27@gmail.com>
2024-08-29 16:56:58 +0700

    refactor(contact data): refactor contact data into single dynamic component

---
+++ src/data/contactData.ts
@@ -0,0 +1,23 @@
+export const contactData = [
+  {
+    contact_id: 1,
+    socmed_name: "Gmail",
+    contact_username: "smmuja27@gmail.com",
+    contact_url: "mailto:smmuja27@gmail.com",
+    contact_icon_url: "/logo/contact/gmail.png",
+  },
+  {
+    contact_id: 2,
+    socmed_name: "LinkedIn",
+    contact_username: "/in/smmuja",
+    contact_url: "https://linkedin.com/in/smmuja/",
+    contact_icon_url: "/logo/contact/linkedin.png",
+  },
+  {
+    contact_id: 3,
+    socmed_name: "GitHub",
+    contact_username: "smmuja",
+    contact_url: "https://github.com/smmuja/",
+    contact_icon_url: "/logo/contact/github.png",
+  },
+];
