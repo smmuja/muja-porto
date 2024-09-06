dfa4b39f8a05b80c72849a05ed7de1a59015a25c
smmuja <smmuja27@gmail.com>
2024-09-06 14:38:19 +0700

    refactor(profile data): base components

--- src/components/base/profileSnippet/index.tsx
+++ src/components/base/profileSnippet/index.tsx
@@ -1,4 +1,5 @@
 import userImg from "assets/img/user.png";
+import { profileData } from "data";
 
 export function ProfileSnippet() {
   return (
@@ -8,18 +9,16 @@ export function ProfileSnippet() {
 
         <div className="flex flex-col gap-1">
           <div className="flex gap-2">
-            <h3 className="font-semibold">Muja (Me)</h3>
+            <h3 className="font-semibold">{profileData.name} (Me)</h3>
             <span className="text-slate-500 text-xs">o</span>
           </div>
           <>
-            <p className="font-light">Software Engineering Associate</p>
+            <p className="font-light">{profileData.role}</p>
           </>
         </div>
       </div>
       <>
-        <p className="font-bold mt-5">
-          Build something beautiful that functions.
-        </p>
+        <p className="font-bold mt-5">{profileData.tagline}</p>
       </>
     </>
   );
