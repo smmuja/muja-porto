0af79b1876941c4783e8b4ec3305b8cb7c78f02d
smmuja <smmuja27@gmail.com>
2025-07-08 02:57:20 +0700

    feat: update work data

--- src/data/workData.ts
+++ src/data/workData.ts
@@ -20,4 +20,36 @@ export const workData = [
     work_end_date: "Jun 2024",
     work_illustration_image_url: "/img/catcode.png",
   },
+  {
+    work_id: 2,
+    work_icon: [
+      {
+        work_icon_id: 1,
+        work_icon_name: "revou.png",
+        work_icon_url: "/logo/work/revou.png",
+      },
+    ],
+    work_company: "RevoU",
+    work_title: "Full Stack Software Engineering (Program)",
+    work_location: "Jakarta (Remote)",
+    work_start_date: "Oct 2023",
+    work_end_date: "Oct 2024",
+    work_illustration_image_url: "/img/catcode.png",
+  },
+  {
+    work_id: 3,
+    work_icon: [
+      {
+        work_icon_id: 1,
+        work_icon_name: "imperial.png",
+        work_icon_url: "/logo/work/imperial.png",
+      },
+    ],
+    work_company: "Imperial Healthtech",
+    work_title: "Software Engineer",
+    work_location: "Semarang",
+    work_start_date: "Jan 2025",
+    work_end_date: "Present",
+    work_illustration_image_url: "/img/catcode.png",
+  },
 ];
