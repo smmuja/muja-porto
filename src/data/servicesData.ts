7841ab640474bc8019c838463e8ba61644dfbafc
smmuja <smmuja27@gmail.com>
2026-05-08 14:00:20 +0700

    assets: work data

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -2,15 +2,15 @@ export const servicesData = [
   {
     service_id: 1,
     service_name: "Webdev Custom",
-    service_url: "https://webdevcustom.com",
+    service_url: "https://www.webdevcustom.com",
     service_icon: "/img/services/webdev-custom.ico",
-    service_description: "Custom web development services",
+    service_description: "Custom web and software development services",
     service_image: "/img/services/webdev-custom.png",
   },
   {
     service_id: 2,
     service_name: "Meownly Career",
-    service_url: "https://meownly.com",
+    service_url: "https://www.meownly.com",
     service_icon: "/img/services/meownly-career.ico",
     service_description: "CV Generator & Career Consultation",
     service_image: "/img/services/meownly-career.png",
